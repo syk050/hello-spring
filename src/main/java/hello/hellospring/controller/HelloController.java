f2a1f1b6b9588fd45f21d4ff23022c14f027a5c2
syk050 <syk050@naver.com>
2022-10-18 15:04:29 +0900

    간단 API 방식

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,5 +24,36 @@ public class HelloController {
         model.addAttribute("name", name);
 
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 응답 바디부분에 데이터를 넣어주겠다
+    public String helloString(@RequestParam("name") String name) {
+        // 뷰 없이 데이터가 그대로 전송
+        // @ResponseBody 를 사용하면 뷰 리졸버( viewResolver )를 사용하지 않음
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody  // default JSON 형태로 반환
+    public  Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+
+    static class Hello {
+        // alt+insert getter setter 생성
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
