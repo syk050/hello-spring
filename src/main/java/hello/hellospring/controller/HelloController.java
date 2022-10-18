b36510459fd61e10434dc6f43dcb2e0666c13e85
syk050 <syk050@naver.com>
2022-10-18 14:26:40 +0900

    간단 MVC 방식

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -15,5 +16,12 @@ public class HelloController {
          * 뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다.
          * resources: templates/ +{ViewName}+ .html    */
         return "hello";
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
     }
 }
