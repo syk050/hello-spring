61622693549368cdea6d75dbd57f2fe8e04dcd62
syk050 <syk050@naver.com>
2022-10-19 13:20:15 +0900

    회원 서비스

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository repository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원X
+//        Optional<Member> result = repository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        repository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 천제 회원 조회
+     */
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Optional<Member> findOne(Long id) {
+        return repository.findById(id);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        repository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+}
