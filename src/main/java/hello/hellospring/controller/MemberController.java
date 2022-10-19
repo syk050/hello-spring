f57d7202e2f56dcddf8c3c96371bfdbcb7aedf51
syk050 <syk050@naver.com>
2022-10-19 16:07:16 +0900

    회원 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 스프링 컨테이너에 등록하여 스프링이 관리
 public class MemberController {
@@ -22,5 +25,20 @@ public class MemberController {
     public MemberController(MemberService service) {
 
         this.service = service;
+    }
+
+    @GetMapping("/members/new")
+    public String createFrom() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        service.join(member);
+
+        return "redirect:/";
     }
 }
