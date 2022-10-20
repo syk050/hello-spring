e13224690c8f73833fe1a1022925b5156776f1f1
syk050 <syk050@naver.com>
2022-10-20 13:56:20 +0900

    Ctoroller에만 AOP 적용

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -13,7 +13,7 @@ public class TimeTraceAop {
 
     // AOP 대상을 지정하는 @Around 코드에서 자기 자신인 TimeTraceAop를 생성하는
     // SpringConfig의 timeTraceAop()도 처리하게 되어 순환참조 문제가 발생
-    @Around("execution(* hello.hellospring..*(..)) && !target(hello.hellospring.SpringConfig)")
+    @Around("execution(* hello.hellospring.controller..*(..)) && !target(hello.hellospring.SpringConfig)")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START : " + joinPoint.toLongString());
